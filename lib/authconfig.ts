2052289fae8a98ebca7b8845e32ff9c915c7ec7a
Himanjal saha <himanjalsaha97@gmail.com>
2024-11-08 23:26:42 +0530

    main functions.

---
+++ lib/authconfig.ts
@@ -0,0 +1,98 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { NextAuthOptions } from "next-auth"
+import NextAuth from "next-auth/next"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { prisma } from "@/lib/prisma"
+import bcrypt from "bcrypt"
+
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+      CredentialsProvider({
+        name: "credentials",
+        credentials: {
+          email: { label: "Email", type: "text" },
+          password: { label: "Password", type: "password" }
+        },
+        async authorize(credentials) {
+          if (!credentials?.email || !credentials?.password) {
+            throw new Error("Invalid credentials")
+          }
+  
+          const user = await prisma.user.findUnique({
+            where: {
+              email: credentials.email
+            },
+            include: {
+              department: true,
+              campus: true,
+              course: true,
+              semester: true
+            }
+          })
+  
+          if (!user || !user?.email || !user?.password) {
+            throw new Error("Invalid credentials")
+          }
+  
+          const isCorrectPassword = await bcrypt.compare(
+            credentials.password,
+            user.password
+          )
+  
+          if (!isCorrectPassword) {
+            throw new Error("Invalid credentials")
+          }
+  
+          return {
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            image: user.image,
+            department: user.department?.name,
+            campus: user.campus?.name,
+            course: user.course?.name,
+            semester: user.semester?.name
+          }
+        }
+      })
+    ],
+    callbacks: {
+      async jwt({ token, user }) {
+        if (user) {
+          token.id = user.id
+          token.email = user.email
+          token.name = user.name
+          token.image = user.image
+          token.department = user.department
+          token.campus = user.campus
+          token.course = user.course
+          token.semester = user.semester
+        }
+        return token
+      },
+      async session({ session, token }) {
+        if (session.user) {
+          session.user.id = token.id as string
+          session.user.email = token.email as string
+          session.user.name = token.name as string
+          session.user.image = token.image as string
+          session.user.department = token.department as string
+          session.user.campus = token.campus as string
+          session.user.course = token.course as string
+          session.user.semester = token.semester as string
+        }
+        return session
+      }
+    },
+    session: {
+      strategy: "jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    debug: process.env.NODE_ENV === "development",
+    pages: {
+      signIn: "/login",
+    }
+  }
+  
